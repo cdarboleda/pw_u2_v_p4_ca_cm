91a53cbcdd3b997f31e6c09ff0c8c17709eda2b8
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-15 19:44:36 -0500

    Taller 17: Vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="HOLA MUNDO"/>
+  <HelloWorld msg="HOLA MUNDO"/>
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
